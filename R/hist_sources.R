8afffdbe7921de1f9d13621bcb5e0b744606466c
ATSzimmat <anton.szimmat@campus.lmu.de>
2025-07-21 23:39:16 +0200

    push

--- R/hist_sources.R
+++ R/hist_sources.R
@@ -17,24 +17,37 @@
 #' # Alle Quellenstellen von Xenophon über den Korinthischen Krieg als Text
 #' Xenophon_Corinthian <- hist_sources(author="Xenophon",context="Corinthian War", output_text=TRUE)
 hist_sources <- function(
-  author = NULL, context = NULL, event_type = NULL, event = NULL, work = NULL,
-  citation = NULL, output_text = FALSE)
-{filtered_sources <- the_sources
-if (!is.null(author)) {
-  filtered_sources <- filter(filtered_sources, author == !!author)}
-if (!is.null(context)) {
-  filtered_sources <- filter(filtered_sources, context == !!context)}
-if (!is.null(event_type)) {
-  filtered_sources <- filter(filtered_sources, event_type == !!event_type)}
-if (!is.null(event)) {
-  filtered_sources <- filter(filtered_sources, event == !!event)}
-if (!is.null(work)) {
-  filtered_sources <- filter(filtered_sources, work == !!work)}
-if (!is.null(citation)) {
-  filtered_sources <- filter(filtered_sources, citation == !!citation)}
-if (output_text) {
-  output <- filtered_sources %>%
-    mutate(formatted = glue("{text_ID} – {citation}\n{trimws(text)}")) %>%
-    pull(formatted)
-  cat(paste(output, collapse = "\n\n"))} else {
-    return(filtered_sources)}}
+    author = NULL, context = NULL, event_type = NULL, event = NULL, work = NULL,
+    citation = NULL, output_text = FALSE, source_data = the_sources)
+{
+  filtered_sources <- source_data
+
+  if (!is.null(author)) {
+    filtered_sources <- dplyr::filter(filtered_sources, author == author)
+  }
+  if (!is.null(context)) {
+    filtered_sources <- dplyr::filter(filtered_sources, context == context)
+  }
+  if (!is.null(event_type)) {
+    filtered_sources <- dplyr::filter(filtered_sources, event_type == event_type)
+  }
+  if (!is.null(event)) {
+    filtered_sources <- dplyr::filter(filtered_sources, event == event)
+  }
+  if (!is.null(work)) {
+    filtered_sources <- dplyr::filter(filtered_sources, work == work)
+  }
+  if (!is.null(citation)) {
+    filtered_sources <- dplyr::filter(filtered_sources, citation == citation)
+  }
+
+  if (output_text) {
+    output <- dplyr::mutate(
+      filtered_sources,
+      formatted = glue::glue("{text_ID} – {citation}\n{trimws(text)}")
+    ) |> dplyr::pull(formatted)
+    cat(paste(output, collapse = "\n\n"))
+  } else {
+    return(filtered_sources)
+  }
+}
