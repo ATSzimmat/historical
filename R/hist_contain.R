8c91261b9af2065f28eff3547617ef5275e02610
ATSzimmat <anton.szimmat@campus.lmu.de>
2025-07-21 17:22:45 +0200

    Push help for hist_contain

--- R/hist_contain.R
+++ R/hist_contain.R
@@ -1,14 +1,25 @@
-#' Title
+#' Filter the source dataframe by the occurrence of different words
 #'
-#' @param data
-#' @param ...
-#'
-#' @returns
+#' @param "words" (The words for which the source dataframe should be filtered)
+#' @returns the filtered source dataframe
 #' @export
 #'
 #' @examples
+#' # Before using hist_cotain, you should have created a dataframe containing the sources you want to examine. To do this, you must have used hist_sources, where output_text must not be set to TRUE.
+#' Xenophon_Corinthian <- hist_sources(author="Xenophon",context="Corinthian War", output_text = FALSE)
 #'
+#' # Filter a source dataframe for sources that contain one specific word, e.g., "king"
+#' Xenophon_Corinthian %>% hist_contain("king")
+#' # Filter a source dataframe for sources that contain several specific words together, e.g. "king", "battle" and "Thebans"
+#' Xenophon_Corinthian %>% hist_contain("king+battle+Thebans")
+#' # Filter a source dataframe for sources where either a specific word or another specific word occurs, e.g., "king" or "Thebans"
+#' Xenophon_Corinthian %>% hist_contain("king","Thebans")
+#' # Filter a source dataframe for sources where e.g. either a specific word or two specific word thogether occur, e.g., "king+battle" or "Thebans"
+#' Xenophon_Corinthian %>% hist_contain("king+battle","Thebans")
 #'
+#' After using hist_cotain you will receive your filtered source dataframe, which you can further edit or convert to a source paper using hist_text
+#' Xenophon_filtered <- Xenophon_Corinthian %>% hist_contain("king+battle","Thebans")
+#' Xenophon_filtered %>% hist_text()
 hist_contain <- function(data, ...) {
   words <- c(...)
   make_filter <- function(entry) {
